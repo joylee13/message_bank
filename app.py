85faceca7579028a7a2f44cfc37f5d280a405f2e
joy-lee <joylee13@ucla.edu>
2023-02-17 21:39:15 -0800

    Debugged Code
    Fixed the rendering for view.html, made a few changes in style.css and added comments to functions in app.py.

--- app.py
+++ app.py
@@ -1,46 +1,44 @@
 from flask import Flask, g, request, render_template
 import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 # render the base template
-
 @app.route("/")
 def base():
     return render_template('base.html')
 
+# function to access the database
 def get_message_db(): 
-    # sql command to create if it doesn't exist yet
-    query = """
+    try:    # return database if exists
+          return g.message_db   
+    except:
+        # create data base if it doesn't exist
+          g.message_db = sqlite3.connect("message.db")
+          cursor = g.message_db.cursor()
+          query = """
             CREATE TABLE IF NOT EXISTS messages (
-            id INTEGER,
+            id INTEGER IDENTITY PRIMARY KEY,
             name TEXT,
             message TEXT
             );
             """
-    try:    # return database if exists
-          return g.message_db   
-    except: # create data base if it doesn't exist
-          g.message_db = sqlite3.connect("message_db.sqlite")
-          cursor = g.message_db.cursor()
           cursor.execute(query)
-
           g.message_db.commit() 
           return g.message_db
 
-
+# function to update database with new submissions
 def insert_message(request):
     conn = get_message_db()               # connect to database
     cursor = conn.cursor()
 
     messages = request.form["message"]    # extract messages
     names = request.form["name"]          # extract names
-    ids = len(g.message_db.index) + 1     # create id numbers
 
     query = """
-       INSERT INTO messages (id, name, message)
-       VALUES ('{},'{}','{}');
-        """.format(ids, names, messages)
+       INSERT INTO messages (name, message)
+       VALUES ('{}','{}');
+        """.format(names, messages)
     cursor.execute(query)
 
     conn.commit()   # save message
@@ -57,11 +55,11 @@ def submit():
             insert_message(request)
             return render_template('submit.html', 
                                     names = request.form["name"],
-                                    messages = request.form["message"],
-                                    thanks = True)
+                                    messages = request.form["message"])
         except:
             return render_template('submit.html', error=True)
 
+# function to view messages
 def random_messages(n):
     query = """
     SELECT * FROM messages ORDER BY RANDOM() LIMIT {};
@@ -75,9 +73,10 @@ def random_messages(n):
     entries = [(row[1], row[2]) for row in rows]
 
     conn.close()
-    return [entries]
+    return entries
 
-@app.route("/view/", methods = ["GET"])
+# render view template
+@app.route("/view/")
 def view():
-    entries = random_messages(1)
+    entries = random_messages(3)
     return render_template('view.html', entries = entries)
